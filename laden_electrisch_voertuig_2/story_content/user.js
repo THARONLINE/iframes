5bd4d4d4ebb5e03add65d405ac308ea80430b9da
THARONLINE <97450210+THARONLINE@users.noreply.github.com>
2022-02-21 10:58:10 +0100

    jh

--- laden_electrisch_voertuig_2/story_content/user.js
+++ laden_electrisch_voertuig_2/story_content/user.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function ExecuteScript(strId)
 {
   switch (strId)
@@ -5,3 +6,12 @@ function ExecuteScript(strId)
   }
 }
 
+=======
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+  }
+}
+
+>>>>>>> 1264d50ad723a8409a772bb62ddade175726907b
